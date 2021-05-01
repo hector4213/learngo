56b3f9a20e6a84ebdb738e946ae9bb5bff36ef85
hector4213 <hectorpwns@gmail.com>
2021-05-01 15:42:54 -0400

    import a package

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"math"
+	"github.com/hector4213/learngo/03_packages/strutil"
 )
 
 func main () {
 fmt.Println(math.Floor(2.7))
 fmt.Println(math.Ceil(2.7))
 fmt.Println(math.Sqrt(36))
+fmt.Println(strutil.Reverse("Hello"))
 }
