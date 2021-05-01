e7123f0809c2093b913e967286c49a3b1987652a
hector4213 <hectorpwns@gmail.com>
2021-05-01 15:11:58 -0400

    init commit

---
+++ 02_variables/main.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+
+func main() {
+	// MAIN TYPES
+
+	// string
+	// bool
+	// int
+	// int int8 int16 int32 int64
+	// uint uint8 uint32 uint 64 iuntptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64
+	// complex64 complex128
+
+	//Using var
+  var name string  = "Hector"
+	
+	fmt.Println(name)
+}
