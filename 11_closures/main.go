23520caa5f9f28f9ab1818887ca14e2e2eb73a33
hector4213 <hectorpwns@gmail.com>
2021-05-02 19:33:52 -0400

    closures examples

---
+++ 11_closures/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func multiplier () func(int) int {
+	product:= 1
+	return func (x int) int {
+		product *= x
+		return product
+	}
+}
+
+func main() {
+	sum := adder()
+	products := multiplier()
+
+	for i := 1; i < 10; i++ {
+		fmt.Println(products(i))
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(sum(i))
+	}
+}
