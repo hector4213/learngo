d337b1c4c7c446b7615b98142bedfb96140760a5
hector4213 <hectorpwns@gmail.com>
2021-05-02 14:45:43 -0400

    conditionals examples

---
+++ 06_conditionals/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+
+func main () {
+	x := 4 
+	y := 25
+
+
+	// If else
+	if x < y {
+		fmt.Printf("%d is less than %d\n", x, y)
+	} else {
+		fmt.Printf("%d is less than %d\n", y, x)
+	}
+
+	// Else if
+
+	color := "pink"
+
+	if color == "orange" {
+		fmt.Println("color is red")
+	} else if color == "pink" {
+		fmt.Println("color is pink")
+	} else {
+		fmt.Println("What kind of color is this")
+	}
+
+	// Switch
+
+	switch color {
+	case "red":
+		fmt.Println("color is red")
+	case "blue":
+		fmt.Println("color is blue")
+	case "pink":
+		fmt.Println("color is pink")
+	default:
+		fmt.Println("what color is this???")
+	}
+
+
+
+}
